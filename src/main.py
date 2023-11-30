70e40604dea90aa5c7601132c65f8d751416b280
Artyom Sokolov <15012003artem@mail.ru>
2023-11-30 15:54:42 +0500

    Add CORS origin

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth import models as auth_models
 from auth.router import router as auth_router
@@ -22,6 +23,16 @@ app.include_router(auth_router)
 app.include_router(group_router)
 app.include_router(child_router)
 app.include_router(parent_router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
